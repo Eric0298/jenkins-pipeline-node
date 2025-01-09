b0af306943f9a3cde0f97862718894b4188d0aae
Eric Mancebo <ericmancebo6950@gmail.com>
2025-01-10 00:01:08 +0100

    modificacio

--- ScriptsJenkins/sendTelegram.js
+++ ScriptsJenkins/sendTelegram.js
@@ -1,13 +1,20 @@
-const TelegramBot = require('node-telegram-bot-api')
+const TelegramBot = require('node-telegram-bot-api');
 const chatID = process.argv[3];
 const botTOKEN = '7864713946:AAG5UjMYTm39SdLAALunWhqWVe2oWF80tyQ';
-const bot = new TelegramBot(botTOKEN, {polling: true});
+const bot = new TelegramBot(botTOKEN, {polling: false});
 const message = process.argv[2];
 
+if (!chatID || chatID === 'numero_chat') {
+    console.error("Error: chatID no proporcionado o inválido.");
+    process.exit(1);
+}
+
 bot.sendMessage(chatID, message)
-    .then(data =>{
-        console.log('Telegram enviado')
-            process.exit(0)
-        
-    } )
-    .catch(e => process.exit(1));
+    .then(() => {
+        console.log('Mensaje enviado con éxito a Telegram');
+        process.exit(0);
+    })
+    .catch((error) => {
+        console.error(`Error enviando mensaje: ${error.message}`);
+        process.exit(1);
+    });
